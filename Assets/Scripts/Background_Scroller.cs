7d146f34ceeece202baa1ee382418ea571f8c48b
Harun <96764147+telestia@users.noreply.github.com>
2023-02-04 05:38:36 +0300

    background dev

---
+++ Assets/Scripts/Background_Scroller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Scroller : MonoBehaviour
+{
+    [Range(-1f, 5f)]
+    public float scrollSpeed = 1f;
+    private float offset;
+    private Material mat;
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        
+        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Player_Controller : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    [SerializeField] private GameObject ground;
+    [SerializeField] private GameObject background;
+    private float backgroundWidth;
+    private float backgroundHeight;
+    private float playerWidth;
+    private float playerHeight;
     private float moveVertical;
+    private float moveHorizontal;
     private float y;
+    private float x;
     [SerializeField] public float moveSpeed;
     
     
@@ -16,27 +22,35 @@ public class Player_Controller : MonoBehaviour
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         moveSpeed = 7f;
-        
+        playerWidth = GetComponent<Renderer>().bounds.size.y;
+        playerHeight = GetComponent<Renderer>().bounds.size.x;
+        backgroundWidth = background.GetComponent<Renderer>().bounds.size.y;
+        backgroundHeight = background.GetComponent<Renderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         moveVertical = Input.GetAxisRaw("Vertical");
-
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
     }
 
     void FixedUpdate()
     {
 
-        if(moveVertical > 0f || moveVertical < 0f ) 
+        y = moveSpeed * moveVertical * Time.deltaTime + transform.position.y;
+        x = moveSpeed * moveHorizontal * Time.deltaTime + transform.position.x;
+
+ 
+        if(!(y < (backgroundWidth / 2 - playerWidth / 2) && y > -(backgroundWidth / 2 - playerWidth / 2)))
         {
-            y = moveSpeed * moveVertical * Time.deltaTime + transform.position.y;
-            
-            if(y > (ground.transform.position.y - ground.transform.localScale.y / 2f + transform.localScale.y / 2) 
-            && y < ground.transform.position.y + ground.transform.localScale.y / 2f - transform.localScale.y / 2  )
-            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            y = transform.position.y;
+        }
+        if(!(x < (backgroundHeight / 2 - playerHeight / 2 - 0.3) && x > -(backgroundHeight / 2 - playerHeight / 2 - 0.3)))
+        {
+            x = transform.position.x;
         }
 
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 }
